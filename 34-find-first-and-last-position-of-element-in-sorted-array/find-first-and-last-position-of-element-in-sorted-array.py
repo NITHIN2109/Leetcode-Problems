9a0e9b597216d542e96eb54a7d0c95c254c33ec9
Alampally Nithin kumar <110482153+NITHIN2109@users.noreply.github.com>
2026-09-07 01:22:51 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (33.10%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -6,26 +6,49 @@ class Solution(object):
         :rtype: List[int]
         """
         n=len(nums)
-        lb=self.LowerBound(nums,target,n)
-        if lb == n or nums[lb]!=target:
-            return [-1,-1]
-        return [lb,self.UpperBound(nums,target,n)-1]
+        # lb=self.LowerBound(nums,target,n)
+        # if lb == n or nums[lb]!=target:
+        #     return [-1,-1]
+        # return [lb,self.UpperBound(nums,target,n)-1]
+        First=self.FirstAndLast(nums,target,True)
+        Last=self.FirstAndLast(nums,target,False)
+        return [First,Last]
 
-    def LowerBound(self,nums,target,n):
-        low,high=0,n-1
-        while low<=high:
-            mid= (low+high) // 2 
-            if nums[mid]>=target:
+    # def LowerBound(self,nums,target,n):
+    #     low,high=0,n-1
+    #     while low<=high:
+    #         mid= (low+high) // 2 
+    #         if nums[mid]>=target:
+    #             high=mid-1
+    #         else :
+    #             low=mid+1
+    #     return low
+    # def UpperBound(self,nums,target,n):
+    #     low,high=0,n-1
+    #     while low<=high:
+    #         mid= (low+high) // 2 
+    #         if nums[mid]>target:
+    #             high=mid-1
+    #         else :
+    #             low=mid+1
+    #     return low
+
+
+    def FirstAndLast(self,nums,target,isFirst):
+        low,high=0,len(nums)-1
+        ans=-1
+        while low<=high :
+            mid =(low+high)//2
+            if nums[mid]==target:
+                ans=mid
+                if isFirst:
+                    high=mid-1
+                else :
+                    low=mid+1
+            elif nums[mid]>target :
                 high=mid-1
             else :
                 low=mid+1
-        return low
-    def UpperBound(self,nums,target,n):
-        low,high=0,n-1
-        while low<=high:
-            mid= (low+high) // 2 
-            if nums[mid]>target:
-                high=mid-1
-            else :
-                low=mid+1
-        return low
+        return ans
+    
+        
