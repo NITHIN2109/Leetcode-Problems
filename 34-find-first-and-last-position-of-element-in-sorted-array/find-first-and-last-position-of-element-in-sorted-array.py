7e90e0f58c4324a79146f7c1b4cef37518df1db5
Alampally Nithin kumar <110482153+NITHIN2109@users.noreply.github.com>
2026-09-07 00:52:45 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (33.27%) - LeetSync

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        n=len(nums)
+        lb=self.LowerBound(nums,target,n)
+        if lb == n or nums[lb]!=target:
+            return [-1,-1]
+        return [lb,self.UpperBound(nums,target,n)-1]
+
+    def LowerBound(self,nums,target,n):
+        low,high=0,n-1
+        while low<=high:
+            mid= (low+high) // 2 
+            if nums[mid]>=target:
+                high=mid-1
+            else :
+                low=mid+1
+        return low
+    def UpperBound(self,nums,target,n):
+        low,high=0,n-1
+        while low<=high:
+            mid= (low+high) // 2 
+            if nums[mid]>target:
+                high=mid-1
+            else :
+                low=mid+1
+        return low
