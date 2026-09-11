77a051b953c18fafe4be11f2359e51f55f6dbd11
Alampally Nithin kumar <110482153+NITHIN2109@users.noreply.github.com>
2026-09-11 16:27:40 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (35.75%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -9,12 +9,12 @@ class Solution(object):
         ans=float('inf')
         while(low<=high):
             mid=(low+high)//2
-            if(ans>nums[mid]):
-                ans=nums[mid]
             
-            if nums[low]<=nums[mid] and nums[mid]>nums[high]:
+            if nums[low]<=nums[mid]:
+                ans=min(ans,nums[low])
                 low=mid+1
             else :
+                ans=min(ans,nums[mid])
                 high=mid-1
         return ans
 
