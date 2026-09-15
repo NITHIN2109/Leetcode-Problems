6e06f3a5ccdf117d6bc969a2e87b808745d1379f
Alampally Nithin kumar <110482153+NITHIN2109@users.noreply.github.com>
2026-09-15 13:44:58 +0530

    Time: 2 ms (67.17%) | Memory: 12.5 MB (19.65%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -7,13 +7,13 @@ class Solution(object):
 
         low=0
         high=x
+        ans=0
         while(low<=high):
             mid=(low+high)//2 
-            if mid*mid==x:
-                return mid
-            elif mid*mid<x:
+            if mid*mid<=x:
+                ans=mid
                 low=mid+1
             else :
                 high=mid-1
-        return high
+        return ans
         
